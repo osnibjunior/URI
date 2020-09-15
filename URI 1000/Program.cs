0c0c5ed7cab6366568774e18e73171bf98013d70
Osni Baumgartner Junior <osnijunior.x3@gmail.com>
2020-09-14 22:38:49 -0300

    Adicionado exercício 1000

---
+++ URI 1000/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI_1000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /**Bem-vindo ao URI Online Judge!
+             * O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!".
+             * Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+             * 
+             * Entrada
+             * Este problema não possui nenhuma entrada.
+             * 
+             * Saída
+             * Você deve imprimir a mensagem "Hello World!" conforme o exemplo abaixo.*/
+
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
