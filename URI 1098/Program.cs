2beacca7fb2d343135f39dab927c27a3ee86f977
Osni Baumgartner Junior <osnijunior.x3@gmail.com>
2021-04-25 23:23:21 -0300

    URI 1098

---
+++ URI 1098/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_1098
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /**Você deve fazer um programa que apresente a sequencia conforme o exemplo abaixo.
+             *
+             * Entrada
+             * Não há nenhuma entrada neste problema.
+             *
+             * Saída
+             * Imprima a sequencia conforme exemplo abaixo.
+             * 
+             * I=0 J=1
+             * I=0 J=2
+             * I=0 J=3
+             * I=0.2 J=1.2
+             * I=0.2 J=2.2
+             * I=0.2 J=3.2
+             * .....
+             * I=2 J=?
+             * I=2 J=?
+             * I=2 J=?*/
+
+             float I = 0f;
+             float J = 1f;
+
+            for (int e = 0; e < 3; e++){
+                Console.WriteLine("I="+I.ToString(CultureInfo.InvariantCulture)+" J="+J.ToString(CultureInfo.InvariantCulture));
+                J++;
+            }
+
+        }
+    }
+}
